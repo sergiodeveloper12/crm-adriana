d178ab6bcfd682d10219d0829982c201570850ee
Sergio Faria <serginhophn@gmail.com>
2026-08-02 18:00:23 -0300

    cria tabela usuarios e admin inicial

--- backend/app/init_db.py
+++ backend/app/init_db.py
@@ -1,10 +1,19 @@
 from sqlalchemy import text
 from .database import engine
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
 
 
 def criar_tabelas():
 
     with engine.connect() as conn:
+
+        # tabela clientes
         conn.execute(text("""
             CREATE TABLE IF NOT EXISTS clientes (
                 id SERIAL PRIMARY KEY,
@@ -17,6 +26,53 @@ def criar_tabelas():
                 data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
         """))
+
+
+        # tabela usuarios
+        conn.execute(text("""
+            CREATE TABLE IF NOT EXISTS usuarios (
+                id SERIAL PRIMARY KEY,
+                usuario VARCHAR(50) UNIQUE NOT NULL,
+                senha_hash VARCHAR(255) NOT NULL,
+                perfil VARCHAR(50) DEFAULT 'admin'
+            );
+        """))
+
+
+        # cria usuário admin se não existir
+        usuario_existente = conn.execute(
+            text("""
+                SELECT id
+                FROM usuarios
+                WHERE usuario='admin'
+            """)
+        ).fetchone()
+
+
+        if not usuario_existente:
+
+            senha = pwd_context.hash("123456")
+
+            conn.execute(
+                text("""
+                    INSERT INTO usuarios
+                    (
+                        usuario,
+                        senha_hash,
+                        perfil
+                    )
+                    VALUES
+                    (
+                        'admin',
+                        :senha,
+                        'admin'
+                    )
+                """),
+                {
+                    "senha": senha
+                }
+            )
+
 
         conn.commit()
 
