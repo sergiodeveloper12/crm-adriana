5aa5c2073c0d54acce2f80482c46ff93cd7afbe2
Sergio Faria <serginhophn@gmail.com>
2026-08-02 17:19:33 -0300

    executa criacao da tabela no inicio da API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from .init_db import criar_tabelas
 from .auth import router as auth_router
 from .clientes import router as clientes_router
@@ -10,33 +11,4 @@ app = FastAPI(
     version="1.0.0"
 )
 
-from .init_db import criar_tabelas
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8082"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-app.include_router(
-    auth_router,
-    prefix="/api"
-)
-
-
-app.include_router(
-    clientes_router,
-    prefix="/api"
-)
-
-
-@app.get("/")
-def home():
-    return {
-        "status": "CRM Adriana Froes API online"
-    }
+criar_tabelas()
