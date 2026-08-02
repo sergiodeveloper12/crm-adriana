42a4f73d78b5e1143b1b3e81dc251b0d48691fe1
Sergio Faria <serginhophn@gmail.com>
2026-08-02 16:51:42 -0300

    Usar DATABASE_URL do Render

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,15 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://projeto:Projeto%402026%23@localhost:5433/crm_adriana"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
 
 Base = declarative_base()
 
