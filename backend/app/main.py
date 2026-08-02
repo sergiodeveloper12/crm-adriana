fd3dccbd9201a37a2d01abda78aa1da0bfc9ad89
Sergio Faria <serginhophn@gmail.com>
2026-08-02 17:30:06 -0300

    corrige rotas da API

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,9 +6,38 @@ from .auth import router as auth_router
 from .clientes import router as clientes_router
 
 
+criar_tabelas()
+
+
 app = FastAPI(
     title="CRM Adriana Froes API",
     version="1.0.0"
 )
 
-criar_tabelas()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(
+    auth_router,
+    prefix="/api"
+)
+
+
+app.include_router(
+    clientes_router,
+    prefix="/api"
+)
+
+
+@app.get("/")
+def home():
+    return {
+        "status": "CRM Adriana Froes API online"
+    }
